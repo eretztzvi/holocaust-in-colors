26a8a10e75f5cf19a0c3d0bbd9a61d47e0a3268b
amir <amirisrael22@gmail.com>
2022-03-10 19:55:03 +0200

    login action

--- src/components/Auth/LoginDialog.js
+++ src/components/Auth/LoginDialog.js
@@ -2,17 +2,15 @@ import React from 'react'
 import { Card, Chip, Dialog, Divider, Typography } from '@mui/material'
 import GoogleLogin from 'react-google-login'
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
-
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 export default function LoginDialog({ open, onClose }) {
 
     const styles = {
         card: {
-            width: '500px',
-            height: '600px',
-            // display: 'flex',
+            width: '400px',
             flexDirection: 'column',
-            // alignItems: 'center',
             padding: '20px'
         },
         title: {
@@ -20,22 +18,50 @@ export default function LoginDialog({ open, onClose }) {
         },
         googleBtn: {
             width: '100%',
-            height: '40px',
-            margin: '30px 0'
+            height: '50px',
+            margin: '30px 0',
+            display: 'flex',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            fontSize: '15px',
+            backgroundColor: '#DB4437',
+            color: 'white',
+            outline: 'none',
+            border: 'none',
+            borderRadius: '10px',
+            cursor: 'pointer'
         },
         facebookBtn: {
             width: '100%',
-            height: '40px',
-            margin: '30px 0'
+            height: '50px',
+            margin: '30px 0',
+            display: 'flex',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            fontSize: '15px',
+            backgroundColor: '#4267B2',
+            color: 'white',
+            outline: 'none',
+            border: 'none',
+            borderRadius: '10px',
+            cursor: 'pointer'
         },
     }
 
     const responseGoogle = (response) => {
         console.log(response.profileObj);
+        handleLocalStorage(response.profileObj)
+        onClose()
     }
 
     const responseFacebook = (response) => {
         console.log(response);
+        handleLocalStorage(response)
+        onClose()
+    }
+
+    const handleLocalStorage = (data) => {
+        localStorage.setItem('myUser', JSON.stringify(data))
     }
 
     return (
@@ -46,13 +72,12 @@ export default function LoginDialog({ open, onClose }) {
                 <GoogleLogin
                     clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
                     render={renderProps => (
-                        <button style={styles.googleBtn} onClick={renderProps.onClick} disabled={renderProps.disabled}>התחברות עם גוגל</button>
+                        <button style={styles.googleBtn} onClick={renderProps.onClick} disabled={renderProps.disabled}>התחברות עם גוגל &nbsp; <GoogleIcon /></button>
                     )}
                     onSuccess={responseGoogle}
                     onFailure={responseGoogle}
                     cookiePolicy={'single_host_origin'}
                 />
-                <br />
                 <Divider >
                     <Chip label="או" />
                 </Divider>
@@ -61,10 +86,9 @@ export default function LoginDialog({ open, onClose }) {
                     appId="1319954525156071"
                     autoLoad={true}
                     render={renderProps => (
-                        <button style={styles.facebookBtn} onClick={renderProps.onClick}>התחברות עם פייסבוק</button>
+                        <button style={styles.facebookBtn} onClick={renderProps.onClick}>התחברות עם פייסבוק &nbsp; <FacebookIcon /></button>
                     )}
                     fields="name,email,picture"
-                    // onClick={componentClicked}
                     callback={responseFacebook} />
 
             </Card>
